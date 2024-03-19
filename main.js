49a5bace93cef373eb0e2c67969158eefdb4aeaf
shukhratgithub <137334308+shukhratgithub@users.noreply.github.com>
2024-03-20 03:26:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -20,4 +20,56 @@ const data = [
       "icon": "./assets/images/icon-visual.svg"
     }
   ];
+
   
+  
+  document.addEventListener("DOMContentLoaded", () => {
+
+      const elStatsList = document.querySelector('.stats-list');
+      const elStatsContentFragment = document.createDocumentFragment();
+
+      data.forEach(function (item) {
+
+        const elStatsItem = document.createElement('li');
+        elStatsItem.classList.add('stats-list_item', `stats-list_item--${item.category.toLowerCase()}`);
+
+        const elStatsListImg = document.createElement('img');
+        elStatsListImg.classList.add('stats-list_item-icon');
+        elStatsListImg.src = item.icon;
+        elStatsListImg.alt = "";
+        elStatsListImg.width = "20  ";
+        elStatsListImg.height = "20";
+        elStatsListImg.ariaHidden = true;
+
+        const elStatsListTitle = document.createElement('span');
+        elStatsListTitle.classList.add('stats-list_item-title');
+        elStatsListTitle.textContent = item.category;
+
+
+        const elStatsListResult = document.createElement('span');
+        elStatsListResult.classList.add('stats-list_item-result');
+        elStatsListResult.textContent = item.score;
+
+
+        const elStatsListPercentage = document.createElement('span');
+        elStatsListPercentage.classList.add('stats-list_item-percentage');
+        elStatsListPercentage.textContent = "%";
+
+
+        const elStatsListMax = document.createElement('span');
+        elStatsListMax.classList.add('stats-list_item-max');
+        elStatsListMax.textContent = "/ 100";
+
+        elStatsItem.appendChild(elStatsListImg);
+        elStatsItem.appendChild(elStatsListTitle);
+        elStatsItem.appendChild(elStatsListResult);
+        elStatsItem.appendChild(elStatsListPercentage);
+        elStatsItem.appendChild(elStatsListMax);
+
+
+        elStatsContentFragment.appendChild(elStatsItem);
+
+      });
+
+      elStatsList.appendChild(elStatsContentFragment);
+    }); 
